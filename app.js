cb8f800653bdffe79496cc126cc4498ead8776f9
Jacob Johnston <johnstonajacob@gmail.com>
2018-06-12 14:01:39 -0500

    oauth callback fix and add to db

--- app.js
+++ app.js
@@ -13,35 +13,32 @@ mongoose
   .catch(console.error);
 
 const port = process.env.PORT || 8080;
-
 const app = express();
 app.set('view engine', 'ejs');
 app.use(
   require('express-session')({
     secret: 'this is a dope secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use( (req, res, next) => {
-	console.log(`SERVING ${req.method} REQUEST ON ${req.url}`);
-	next();
-})
+app.use((req, res, next) => {
+  console.log(`SERVING ${req.method} REQUEST ON ${req.url}`);
+  next();
+});
 passport.use(new localStrategy(User.authenticate()));
 passport.use(
   new TwitterStrategy(
     {
       consumerKey: process.env.TWITTER_KEY,
       consumerSecret: process.env.TWITTER_SECRET,
-      callbackURL: 'http://206.189.170.211/auth/twitter/callback'
+      callbackURL: 'http://206.189.170.211/auth/twitter/callback',
     },
     (token, tokenSecret, profile, cb) => {
-      User.findOrCreate({ twitterId: profile.id }, (err, user) =>
-        cb(err, user)
-      );
+      User.findOrCreate({ twitterId: profile.id }, (err, user) => cb(err, user));
     }
   )
 );
@@ -64,20 +61,16 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
   const body = req.body;
-  User.register(
-    new User({ username: body.username }),
-    body.password,
-    (err, user) => {
-      if (err) {
-        console.error(err);
-        return res.render('register');
-      } else {
-        passport.authenticate('local')(req, res, () => {
-          res.redirect('/secret');
-        });
-      }
+  User.register(new User({ username: body.username }), body.password, (err, user) => {
+    if (err) {
+      console.error(err);
+      return res.render('register');
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/secret');
+      });
     }
-  );
+  });
 });
 
 app.get('/login', (req, res) => {
@@ -88,7 +81,7 @@ app.post(
   '/login',
   passport.authenticate('local', {
     successRedirect: '/secret',
-    failureRedirect: '/login'
+    failureRedirect: '/login',
   }),
   (req, res) => {}
 );
--- models/user.js
+++ models/user.js
@@ -4,7 +4,26 @@ const UserSchema = new mongoose.Schema({
   username: String,
   passowrd: String,
 });
-
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+
+User.findOrCreate = function(info) {
+  console.log(info);
+  User.findOne({ username: info.twitterId }, (err, user) => {
+    if (err) return console.error(err);
+    if (!user) {
+      user = new User({
+        username: user.twitterId,
+      });
+      user.save((err) => {
+        if (err) console.error(err);
+        else return user;
+      });
+    } else {
+      return user;
+    }
+  });
+};
+
+module.exports = User;
