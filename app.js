d0b7c29a8bbb0510c86290eac1c82bb8a6fd8522
jacob <jacob@johnstonjacob.com>
2018-06-12 18:43:49 +0000

    twitter auth button

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   localStrategy = require('passport-local'),
   passportLocalMongoose = require('passport-local-mongoose'),
-  User = require('./models/user');
-
+  dotenv = require('dotenv').config({ silent: true }),
+  User = require('./models/user'),
+  TwitterStrategy = require('passport-twitter');
 mongoose
   .connect('mongodb://206.189.170.211/auth')
   .then(() => console.log('Connection Succeeded'))
   .catch(console.error);
+
+const port = process.env.PORT || 8080;
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -17,13 +20,31 @@ app.use(
   require('express-session')({
     secret: 'this is a dope secret',
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: false
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use( (req, res, next) => {
+	console.log(`SERVING ${req.method} REQUEST ON ${req.url}`);
+	next();
+})
 passport.use(new localStrategy(User.authenticate()));
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: process.env.TWITTER_KEY,
+      consumerSecret: process.env.TWITTER_SECRET,
+      callbackURL: 'http://206.189.170.211/auth/twitter/callback'
+    },
+    (token, tokenSecret, profile, cb) => {
+      User.findOrCreate({ twitterId: profile.id }, (err, user) =>
+        cb(err, user)
+      );
+    }
+  )
+);
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -43,16 +64,20 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
   const body = req.body;
-  User.register(new User({ username: body.username }), body.password, (err, user) => {
-    if (err) {
-      console.error(err);
-      return res.render('register');
-    } else {
-      passport.authenticate('local')(req, res, () => {
-        res.redirect('/secret');
-      });
+  User.register(
+    new User({ username: body.username }),
+    body.password,
+    (err, user) => {
+      if (err) {
+        console.error(err);
+        return res.render('register');
+      } else {
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/secret');
+        });
+      }
     }
-  });
+  );
 });
 
 app.get('/login', (req, res) => {
@@ -63,7 +88,7 @@ app.post(
   '/login',
   passport.authenticate('local', {
     successRedirect: '/secret',
-    failureRedirect: '/login',
+    failureRedirect: '/login'
   }),
   (req, res) => {}
 );
@@ -73,6 +98,14 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get(
+  '/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  (req, res) => res.redirect('/secret')
+);
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
@@ -80,6 +113,6 @@ function isLoggedIn(req, res, next) {
   res.redirect('/');
 }
 
-app.listen(8080, 'localhost', () => {
+app.listen(port, 'localhost', () => {
   console.log('Server Started');
 });
